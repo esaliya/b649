fbd15f525730e078895e614c076fea3b4ecbca00
Saliya Ekanayake <esaliya@gmail.com>
2016-01-21 12:36:44 -0500

    fixing run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac edu.indiana.cs.b649.wc.serial.SerialWordCount.java
-java edu.indiana.cs.b649.wc.serial.SerialWordCount $1
+javac ../src/main/java/edu/indiana/cs/b649/wc/serial/SerialWordCount.java
+java -cp ../src/main/java edu.indiana.cs.b649.wc.serial.SerialWordCount ../src/main/resources/words.txt
