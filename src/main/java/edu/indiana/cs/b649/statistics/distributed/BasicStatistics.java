0bd745e1b0152c455bebe48449e81c65728d2566
Saliya Ekanayake <esaliya@gmail.com>
2016-01-31 22:30:53 -0500

    Fixing minor bug

--- src/main/java/edu/indiana/cs/b649/statistics/distributed/BasicStatistics.java
+++ src/main/java/edu/indiana/cs/b649/statistics/distributed/BasicStatistics.java
@@ -1,23 +1,17 @@
 package edu.indiana.cs.b649.statistics.distributed;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 import java.io.IOException;
-import java.text.DecimalFormat;
 
 public class BasicStatistics {
     private static Text k = new Text("k");
