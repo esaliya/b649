fbff7fad19b741fb99d18c4be417883ef5619e33
Saliya Ekanayake <esaliya@gmail.com>
2016-04-20 23:47:08 -0400

    Adding spark test

--- src/main/java/edu/indiana/cs/b649/frameworks/spark/SparkPrimer.java
+++ src/main/java/edu/indiana/cs/b649/frameworks/spark/SparkPrimer.java
@@ -7,7 +7,7 @@ import org.apache.spark.api.java.JavaSparkContext;
 import java.util.Arrays;
 import java.util.List;
 
-public class SparkTest {
+public class SparkPrimer {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("HelloWorld");
         JavaSparkContext sc = new JavaSparkContext(conf);
